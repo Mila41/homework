0033f0e8a17f4afbcd68afdec3aca9f2c9a7f996
Mila41 <114509046+Mila41@users.noreply.github.com>
2022-11-18 13:26:29 +0300

    my version

---
+++ number1.py
@@ -0,0 +1,11 @@
+my_favorite_songs = "wast a moment", "a sorta fairytale", "new salvation", "start me up", "staying alive"
+greeting = "my_favorite_songs = a,b,c,d,e"
+a = " wast a moment"
+b = " new salvation"
+c = " staying alive"
+d = " start me up"
+e = " a sorta fairytale"
+fvar = greeting[:20]
+svar = (a + b + c + d + e)
+greeting[20:29:2]
+print(fvar + svar)
